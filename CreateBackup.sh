72c5639c5a5d6c19edaf56fd5322a2b84495a14a
fancystuff4 <68031547+fancystuff4@users.noreply.github.com>
2023-04-16 14:27:38 +0530

    first

---
+++ CreateBackup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Create mysql backup file
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+filename=MySql-Backup-$timestamp.sql
+
+sudo mysqldump -u "root" -p"admin" --all-databases > ./backups/$filename
+
+echo "Created backup file $filename "
+
+
+#Upload to S3
+sudo aws s3 cp ./backups/$filename s3://db-backup-files-mysql
