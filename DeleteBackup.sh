054a3c8d5cd0b5643ae9ba26a85ed637edb5b67c
fancystuff4 <68031547+fancystuff4@users.noreply.github.com>
2023-04-16 22:33:52 +0530

    Update DeleteBackup.sh

--- DeleteBackup.sh
+++ DeleteBackup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 cd /home/ubuntu/backups
+
 # Delete files older than  1 min +720 +0.5
 find . -name "*.sql" -type f -mmin +720 -exec rm {} \;
-
-
 
 BUCKET_NAME="db-backup-files-mysql"
 
@@ -16,11 +16,15 @@ file_list=$(aws s3 ls s3://$BUCKET_NAME --recursive | awk '{print $4}')
 # Loop through the file list and delete files older than the time threshold
 for file in $file_list; do
 
+    # Get the file modification time in Unix timestamp
     mod_time=$(aws s3api head-object --bucket $BUCKET_NAME --key $file --query "LastModified" --output text | date -d "$(cat -)" +%s)
 
 
+    # Calculate the age of the file in seconds
     file_age=$(( $(date +%s) - $mod_time ))
 
+
+    # If the file is older than the time threshold, delete it
     if [ $file_age -gt $TIME_THRESHOLD ]; then
         aws s3 rm s3://$BUCKET_NAME/$file
         echo "Deleted file: $file"
